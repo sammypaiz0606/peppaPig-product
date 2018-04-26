7eaa9fbcf081614d90b00b2359ff3f2ba2a79d40
sammy paiz <sammypaiz@sammypaiz.local>
2018-04-25 20:41:28 -0700

    object literal

--- js/jquery.js
+++ js/jquery.js
@@ -1,1 +1,26 @@
+var peppaPig = 
+        {
+            name:'Peppa Pig',
+            age:'4',
+            image:'images/peppapig.jpg',
+            price:15,
+          
+        
+        };
+var pappaPig =
+        {
+            name:'Pappa Pig',
+            age:'5',
+            image:'images/pappapig.jpg',
+            price:7,
+        
+        };
+var georgie =
+        {
+            name:'Georgie',
+            age:'3',
+            image:'images/georgie.jpg',
+            price:5,
+           
+        };
 
